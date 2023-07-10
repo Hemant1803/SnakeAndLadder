bd4eaebf774148b6d7b61a12d31fb982c1a9a425
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-10 19:08:38 +0530

    SnakeAndLaddersUC1

---
+++ SnakeAndLadder/Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Game
+    {
+         public void StartGame()
+        {
+            Console.WriteLine("Start Snake And Ladders");
+            int score = 0;
+            int count = 0;
+            while (score < 100)
+            {
+                int dicevalue = RollDice();
+                count++;
+                int num = score + dicevalue;
+                if (num <= 100)
+                {
+                    score = num;
+                }
+            }         
+                           
+              
+            Console.WriteLine("Dice Role" + count);
+            Console.WriteLine("Score" + score);
+            
+           
+
+        }
+            int RollDice()
+
+            {
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("Random value :"+dice);
+
+                return dice;
+            }
+       
+    }
+}
