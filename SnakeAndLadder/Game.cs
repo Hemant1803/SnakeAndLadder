76ce2a8f9e8aa65d61568f0d5d1b91626d8e9ca1
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-10 21:00:53 +0530

    Snake Ladders UC2

--- SnakeAndLadder/Game.cs
+++ SnakeAndLadder/Game.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 
 namespace SnakeAndLadder
 {
@@ -18,27 +19,43 @@ namespace SnakeAndLadder
             {
                 int dicevalue = RollDice();
                 count++;
-                int num = score + dicevalue;
-                if (num <= 100)
+                
+               
+                int condition = Action();
+                Console.WriteLine("Action value: " + condition);
+                switch (condition)
                 {
-                    score = num;
-                }
-            }         
-                           
-              
-            Console.WriteLine("Dice Role" + count);
-            Console.WriteLine("Score" + score);
-            
-           
+                    case 0: //No Play
+                         break;
+                    case 1: // 1 = ladder
+                        int val = score + dicevalue;
+                        if (val <= 100)
+                        {
+                            score = val;
+                        }
+                        break;
+                        case 2: // 2 = Snake
+                        int val2 = score - dicevalue;
+                        score = val2 >= 0 ? val2 : 0;
+                        break;
 
+                }
+            }
+            Console.WriteLine("Dice Role: " + count);
+            Console.WriteLine("Score: " + score);
+            }
+           int Action()
+        {
+            Random random = new Random();
+            int action = random.Next(0,3);
+            return action;
+            
         }
             int RollDice()
-
             {
                 Random random = new Random();
                 int dice = random.Next(1, 7);
-                Console.WriteLine("Random value :"+dice);
-
+                Console.WriteLine("Random value: " + dice);
                 return dice;
             }
        
