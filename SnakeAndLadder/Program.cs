522de88126117a32e3589174ac0c1a1e58156849
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-10 19:06:37 +0530

    SnakeAndLadders

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladders");
+            Game game = new Game();
+
+            game.StartGame();
+
+        }
+    }
+}
